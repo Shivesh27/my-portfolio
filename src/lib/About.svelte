59cd7f3cdea5b0b67c3da612c4316eb08bbb4df0
Sai Sharana Shivesh <shivesh27@Shivs-Kakashi.local>
2024-12-28 17:51:33 +0530

    Portfolio- landing and about pages

---
+++ src/lib/About.svelte
@@ -0,0 +1,30 @@
+<div class="about-me">
+    <div class="about-container">
+        <div class="title">About Me.</div>
+        <div class="about-content">I'm a frontend developer with a knack for creating seamless user experiences, blending intuitive design with robust functionality. I thrive on solving complex challenges and bringing innovative ideas to life.</div>
+    </div> 
+</div>
+
+
+<style>
+    .about-me {
+        height: 100vh;
+        color: #7fc8f8;
+    }
+    .about-container {
+        text-align: center
+    }
+    .title {
+        font-size: 3rem;
+        padding: 10vh;
+        padding-top: 15vh;
+        font-style: oblique;
+    }
+    .about-content {
+        padding-left: 10vw;
+        padding-right: 10vw;
+        font-size: 4rem;
+        font-weight: 600;
+    }
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+
+	import About from "$lib/About.svelte";
+
+</script>
+
+    <div class="hero-container">
+        <div class="name-container">
+            <div class="name">Sai Sharana Shivesh</div>
+            <div class="subHeading">Frontend Developer</div>
+        </div> 
+    </div>
+
+<About/>
+
+
+
+
+<style>
+    :global(body) {
+        font-family: "PT Serif", serif;
+        font-weight: 400;
+        font-style: normal;
+        background-color: #f9f9f9;
+    }
+    .hero-container {
+        width: 80%;
+        height: 100vh;
+        display: flex;
+        background-color: #7fc8f8;
+        align-items: flex-end;
+        justify-content: flex-end;
+    }
+    .name-container {
+        padding: 5vh;
+        padding-bottom: 12vh;
+        width: 50%;
+        text-align: right;
+    }
+    .name {
+        font-size: 2rem;
+        font-weight: 600;
+        color: #f9f9f9;
+    }
+    .subHeading {
+        font-size: 1rem;
+        font-style: italic;
+        font-weight: 400;
+        color: #f9f9f9;
+    }
+
+    @media (min-width: 640px) {
+        .name {
+            font-size: 3.5rem;
+        }
+        .subHeading {
+            font-size: 1.7rem;
+        }
+    }
+    @media (min-width: 1280px) {
+        .name {
+            font-size: 6rem;
+        }
+        .subHeading {
+            font-size: 2.5rem;
+        }
+    }
+</style>
